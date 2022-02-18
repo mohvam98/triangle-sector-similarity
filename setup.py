c22b91e33c270927e662ab23eccc458f8841a8af
mohvam98 <mohanavamsi98@gmail.com>
2022-02-18 18:21:04 +0530

    math dependancy

--- setup.py
+++ setup.py
@@ -22,8 +22,9 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     packages=["triangle-sector-similarity"],
     include_package_data=True,
-    install_requires=['numpy','scipy','math']
+    install_requires=['numpy','scipy']
 )
